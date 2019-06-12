56cfd6f2f113fcc2326ccfe26ba0265d78d14793
Kim Weiss <kiminy@gmail.com>
2019-06-12 17:10:43 -0400

    Create about page and add basic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { Component } from "react"
+import { Switch, Route } from "react-router-dom"
 import './App.css'
-
+import NavBar from "./components/nav_bar"
 import HomePage from './pages/home_page'
+import AboutPage from './pages/about_page'
 
-function App() {
-  return (
-      <div className="App">
-        <HomePage />
-      </div>
-  )
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <NavBar />
+                <main>
+                    <Switch>
+                        <Route exact path='/' component={HomePage} />
+                        <Route path='/home' component={HomePage} />
+                        <Route path='/about' component={AboutPage} />
+                    </Switch>
+                </main>
+            </div>
+        )
+    }
 }
+
 export default App
